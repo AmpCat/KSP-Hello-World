8447c0250f1c4e8f54fb5d60b16074c8dbb26a3b
AmpCat <robertlyork@gmail.com>
2019-10-23 13:16:14 -0500

    Trying to fix repo

---
+++ Hello World.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Hello : MonoBehaviour
+    {
+        public void Update()
+        {
+            Debug.Log("Hello world! " + Time.realtimeSinceStartup);
+        }
+    }
+}
+
+namespace HelloWorld2
+{
+    /// <summary>
+    /// My first part!
+    /// </summary>
+    public class HelloKerbinMod : PartModule
+    {
+        /// <summary>
+        /// Called when the part is started by Unity.
+        /// </summary>
+        public override void OnStart(StartState state)
+        {
+            // Add stuff to the log
+            print("Hello, Kerbin?");
+        }
+    }
+}
